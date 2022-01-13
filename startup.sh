0b34a6b84e507eac791941fc85e78dc7009c4fa6
Justin Zhu <31556466+Zhu-Justin@users.noreply.github.com>
2022-01-13 16:59:57 -0700

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,7 @@
+alias config='/usr/bin/git --git-dir=$HOME/.zhu_config/ --work-tree=$HOME'
+echo ".zhu_config" >> .gitignore
+git clone --bare git@github.com:Zhu-Justin/.zhu_config.git $HOME/.zhu_config
+config checkout
+config config --local status.showUntrackedFiles no
+config push --set-upstream origin master
+source ~/.profile
