435585ddde90836e185fadd5db26d3dadaf4c94e
Justin Zhu <31556466+Zhu-Justin@users.noreply.github.com>
2023-09-23 14:00:54 -0400

    Updated

--- startup.sh
+++ startup.sh
@@ -1,8 +1,13 @@
-shopt -s expand_aliases
-alias config='/usr/bin/git --git-dir=$HOME/.zhu_config/ --work-tree=$HOME'
-echo ".zhu_config" >> .gitignore
-git clone --bare git@github.com:Zhu-Justin/.zhu_config.git $HOME/.zhu_config
-config checkout
-config config --local status.showUntrackedFiles no
-config push --set-upstream origin master
-source ~/.profile
+if [ -f ~/.profile && -d ~/.zhu_config ]; then
+  rm -i ~/.profile
+  rm -i ~/.zhu_config
+else
+  shopt -s expand_aliases
+  alias config='/usr/bin/git --git-dir=$HOME/.zhu_config/ --work-tree=$HOME'
+  echo ".zhu_config" >> .gitignore
+  git clone --bare git@github.com:Zhu-Justin/.zhu_config.git $HOME/.zhu_config
+  config checkout
+  config config --local status.showUntrackedFiles no
+  config push --set-upstream origin master
+  source ~/.profile
+fi
