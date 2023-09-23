c1e0c08e8e4c263db7db54072d2f219f74e84efb
Justin Zhu <31556466+Zhu-Justin@users.noreply.github.com>
2023-09-23 14:30:11 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ elif [ -d ~/.zhu_config ]; then
   rm -i ~/.zhu_config
 else
   git clone --bare git@github.com:Zhu-Justin/.zhu_config.git $HOME/.zhu_config
-  shopt -s expand_aliases
-  alias config='/usr/bin/git --git-dir=$HOME/.zhu_config/ --work-tree=$HOME'
+  shopt -s expand_aliases;
+  alias config='/usr/bin/git --git-dir=$HOME/.zhu_config/ --work-tree=$HOME';
   echo ".zhu_config" >> .gitignore
   config checkout
   config config --local status.showUntrackedFiles no
